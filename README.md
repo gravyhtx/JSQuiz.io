041d4ed2ac95d35fe18f35cdbc73aaa88565c672
GravyHTX <godisgravy@gmail.com>
2020-02-11 21:30:08 -0600

    New update with screen change to username entry by using document.getElementById() and that's pretty much itbut I have been trying very hard...

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # JSQuiz
 JavaScript: The Quiz
+
+Every step in this process will change within the same page by using document.getElementById( ).
+
+Homepage sets all variables and fills in card2 with text. Scoreboard will be blank until a user completes a game.
+
+When the user presses button, page will ask for username. Username will be set and game will start on submit. Timer counts down from 1000 and decreases by 20 every second. There will be 5 questions each with timer increase when correct or decrease when wrong. Each question will deduct 25 counts and correct answer will increase it by 100 + a difficulty factor.
+
+Questions will have 5 answer choices set inside of buttons. Correct answers will display "CORRECT" and incorrect answers will display "NOPE" at bottom of screen. This will last 0.5 seconds before switching to new screen with next question.
+
+If timer reaches 0 or user completes all questions game will end with timer being the user score. Username and score is displayed and a button to continue is clicked to display home page with a screen displaying the score on a scoreboard with the username. Every time the quiz is taken username and score will be added to the scoreboard, displayed in order of highest to lowest scores.
+
+This would be super dope if it actually works...
--- script.js
+++ script.js
@@ -1,48 +1,77 @@
-/*
-Display High Scores, JavaScript: The Quiz
-Click Button to start Quiz
+// JavaScript: The Quiz //
 
-*/
 
+// VARIABLES //
+// function gameStart() {
 var gameOn = false
+if (gameOn == false) {
+    document.getElementById("qNumber").innerHTML = "YOU'RE THE BEST AROUND!";
+    document.getElementById("qQuestion").innerHTML = "HIGH SCORES";
+    document.getElementById("generate").innerHTML = "WANT TO PLAY A GAME?";
+}
+else {
+    document.getElementById("qNumber").innerHTML = "YOU'RE THE BEST AROUND!";
+    document.getElementById("qQuestion").innerHTML = "HIGH SCORES";
+    document.getElementById("generate").innerHTML = "WANT TO PLAY A GAME?";
+}
+// }
+
 var scoreOn = false
+var timer = 1000
 
 console.log(gameOn, scoreOn)
 
 
-var answerIs = {
-    items: ['', 'CORRECT', 'NOPE'],
-}
+var answerIs = ['', 'CORRECT', 'NOPE'];
+var page = ['home', 'q1', 'q2', 'q3', 'q4', 'q5',];
 
 var timerOn = false;
 
-var username
+var username;`  `
+
+var screenIs = false;
+
+
 
 
 // FUNCTIONS //
 
 $("#generate").on("click", function() {
     console.log("I've been clicked!");
+    document.getElementById("generate").innerHTML = "SUBMIT";
+    document.getElementById("qNumber").innerHTML = "Please enter your username...";
+    document.getElementById("qQuestion").innerHTML = "MAX 8 CHARACTERS";
+    // var screenIs = true;
 
-    var userEntry = prompt("Please enter your initials (max 3 char.)");
+    // var screenHTML = 
+    // document.getElementById("screen") = tammy;
+    // function tammy() {
 
+    // }
+    // if (screenIs === true)
+    //     screenIs.
+
+    // document.getElementById("screen").innerHTML {
+    //     <textarea
+    //         id="scoreBoard"
+    //         placeholder="username"
+    //         aria-label="HIGH SCORES"
+    //       ></textarea>
+    // }
     var response;
     if (userEntry.length == 0){
         response = "Ouch!";
         var gameOn = false;
-    } else if (userEntry.length <= 3){
+    } else if (userEntry.length <= 8){
         response = "Let's do this!";
         var gameOn = true;
-    } else if (userEntry.length > 3) {
+    } else if (userEntry.length > 8) {
         response = "Why did you do that?";
         var gameOn = false;
     }
     alert(response);
-    console.log(gameOn);
-
-    function usernameEntry() {
-        
-    }
+    console.log("gameOn " + gameOn);
+    console.log("userEntry " + userEntry);
   });
 
 
@@ -50,31 +79,30 @@ $("#generate").on("click", function() {
 /*
 
 HOME
-- Checks for text in card2
-- Clear timer
+- Set text in card2
+- Set timer to 1000 (or 100 per question)
 - Wait for click
 - Store username
 
 GAME ON
 - Start timer
-    set timer to 1000
     25 time loss per second
     correct answer gains defined time, 100 + different number per question for more randomized score
-    wrong answer lose 25 time
+    wrong answer lose 20 time
     time = score
-- Questions, change text:
+- Questions, change text  >>  document.getElementById(id).innerHTML = NEW TEXT  <<
     id = qNumber to question number
     id = qQuestion to the question
     id = screen to answers, clickable
     Clear card-footer / "btn" (#generate) and show "CORRECT" or "NOPE"
         choose from answerIs.items[0-2]
-- Game ends when timer <= 0 or all questions answered
+- Game ends when:  timer <= 0  or  all questions answered
 
 GAME OFF
 - Score is final time
 - Alert score
-- Add username and score to scoreBoard
-- 
+- Add username and score to scoreBoard in order of score
+- Set timer, wait for click...
 
 */
 
@@ -84,7 +112,6 @@ FUNCTIONS
 
 function scoreBoard (username, score) {
     idk
-    
 }
 
 */
